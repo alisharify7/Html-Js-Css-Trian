08ce6208e1845758df246e72483665b10a1d3761
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-10-10 16:22:41 +0330

    fix some changes

--- accordion/02.Accordion/script.js
+++ accordion/02.Accordion/script.js
@@ -7,15 +7,12 @@ accordions.forEach((each)=>{
         each.addEventListener("click", (e)=> {
             let curent = e.currentTarget;
             
-            let accordion_body = curent.children[1];
-            
-            if(accordion_body.classList.contains("show"))
+            if(curent.classList.contains("show"))
             {
-                accordion_body.classList.toggle("show")
+                curent.classList.toggle("show")
             }
             else{
-                accordion_body.classList.toggle("show")
-                
+                curent.classList.toggle("show")
             }
         })
     }
