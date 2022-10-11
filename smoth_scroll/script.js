0fa4508023bdaf207a2a6f866bd28b3c6f749630
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-10-11 19:57:44 +0330

    added scroll smoth

---
+++ smoth_scroll/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("scroll", function(e) {
+    if(window.scrollY >= 3500)
+    {
+        document.querySelector(".scroll").classList.add("show");
+        document.querySelector(".scroll").addEventListener("click", e => {
+            window.scrollTo({top:0, behavior:"smooth"});
+        })
+        
+    }
+    else
+    {
+        document.querySelector(".scroll").classList.remove("show");
+    }
+})
